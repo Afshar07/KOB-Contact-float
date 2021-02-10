93115c81f91a15e57666361f253d4d7f910935c6
Amir Afshar <mr.afshar0@gmail.com>
2021-02-10 16:23:10 +0330

    change phone icon display  to none when active class is on

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const circle = document.querySelector(".circle");
 const border = document.querySelector(".active");
+const ic = document.querySelector(".fa-phone");
 circle.addEventListener("click", ()=>{
     let ac = circle.classList.toggle("circlicked");
     if(ac == true){
     border.style.display = "block";
+    ic.style.display = "none";
     }else{
         border.style.display = "none"
+        ic.style.display = "block"
     }
 })
