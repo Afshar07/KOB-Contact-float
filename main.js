4e0ae48fe780bc6923178d5796078769002ed5f4
Amir Afshar <mr.afshar0@gmail.com>
2021-02-19 23:39:54 +0330

    Add a function to remove the box when click outside of it

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 const circle = document.querySelector(".circle");
 const border = document.querySelector(".active");
 const ic = document.querySelector(".fa-phone");
+const elem = document.querySelector(".mcontainer");
 circle.addEventListener("click", ()=>{
     let ac = circle.classList.toggle("circlicked");
     if(ac == true){
     border.style.display = "block";
     ic.style.display = "none";
     }else{
-        border.style.display = "none"
-        ic.style.display = "block"
+        border.style.display = "none";
+        ic.style.display = "block";
     }
-})
+    document.addEventListener("click", (event)=>{
+        let isclicked = circle.contains(event.target);
+        if(!isclicked){
+            circle.classList.remove("circlicked");
+            border.style.display = "none";
+            ic.style.display = "block";
+        }
+    })
+});
+
